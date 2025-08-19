aaaf6db9f8438bb7a44c0febb7eb4be30ee128a7
23f3000048 <23f3000048@ds.study.iitm.ac.in>
2025-08-19 14:13:45 +0530

    final

--- assets/js/init.js
+++
@@ -1,9 +0,0 @@
-Reveal.initialize({
-  hash: true,
-  slideNumber: true,
-  transition: 'slide',
-  plugins: [ RevealMarkdown, RevealNotes ],
-  pdfSeparateFragments: false
-});
-
-hljs.highlightAll();
